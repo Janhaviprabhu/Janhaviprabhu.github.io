bad5ce159dee5ee5fdae35691bc914a1e1c076e5
Janhaviprabhu <januprabhu26@gmail.com>
2022-10-20 00:06:04 +0530

    updated

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,33 +1,42 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { Box, Container, Flex, Text } from "@chakra-ui/react"
 import "../App.css"
 export const Navbar=()=>{
   return (
     <>
+      <Box className="menuicon">
+        <HamburgerIcon />
+      </Box>
       <Box className="sidebar">
-        <Box fontFamily={"initial"} p={"60px"} bg={"#1A365D"} fontSize="46px">
+        <Box
+          color={"#e7e6ff"}
+          fontFamily={"initial"}
+          p={"60px"}
+          bg={"#18172e"}
+          fontSize="46px"
+        >
           Janhavi
         </Box>
-        <Box className="middlebar" mt={10}>
-          <Box className="list" >
-            Home
-          </Box>
-          <Box className="list" >
-            About
-          </Box>
-          <Box className="list">
-            Skills
-          </Box>
-          <Box className="list" >
-            Project
-          </Box>
+        <Box color={"#e7e6ff"} className="middlebar" mt={10}>
+          <Box className="list">Home</Box>
+          <Box className="list">About</Box>
+          <Box className="list">Skills</Box>
+          <Box className="list">Project</Box>
           <Box className="list" p={4}>
             Contact
           </Box>
         </Box>
-        <Flex p={"52px"} gap={10} mt={70} bg={"#1A365D"} textAlign={"center"}>
-          <Box bg={"#1A365D"}>Linkedin</Box>
-          <Box bg={"#1A365D"}>Github</Box>
-          <Box bg={"#1A365D"}>Twitter</Box>
+        <Flex
+          color={"#e7e6ff"}
+          p={"52px"}
+          gap={10}
+          mt={70}
+          bg={"#18172e"}
+          textAlign={"center"}
+        >
+          <Box bg={"#18172e"}>Linkedin</Box>
+          <Box bg={"#18172e"}>Github</Box>
+          <Box bg={"#18172e"}>Twitter</Box>
         </Flex>
       </Box>
     </>
