8b7d8b02d2c32cde7415a7457b3b0465858af50b
Janhaviprabhu <januprabhu26@gmail.com>
2023-02-04 16:00:18 +0530

    style added

--- src/App.js
+++ src/App.js
@@ -6,21 +6,15 @@ import { About } from "./Components/About";
 import { Skills } from "./Components/Skills";
 import { Work } from "./Components/Work";
 import { Contact } from "./Components/Contact";
-import { useContext, useEffect, useRef, useState } from "react";
-import { AiFillContacts, AiFillHome } from "react-icons/ai";
-import { SiAboutdotme } from "react-icons/si";
-import { GiSkills } from "react-icons/gi";
-import { RiComputerFill } from "react-icons/ri";
-import { AiFillProfile } from "react-icons/ai";
-import { BsMoonFill } from "react-icons/bs";
-import { BsFillSunFill } from "react-icons/bs";
+import { useContext, useRef} from "react";
 import { AppContex } from "./Context/Authcontext";
 import Simple from "./Components/Navbar";
-
+import CustomCursor from 'custom-cursor-react';
+import 'custom-cursor-react/dist/index.css';
 
 
 function App() {
-  const { theme, setTheme, ToggleTheme } = useContext(AppContex);
+  const { theme } = useContext(AppContex);
   const Profileref = useRef(null);
   const Aboutref = useRef(null);
   const Skillsref = useRef(null);
@@ -46,7 +40,13 @@ function App() {
 
   return (
     <div style={theme === "light" ? light : dark} className="App">
-  
+      <CustomCursor targets={['.link', '.your-css-selector']} customClass='custom-cursor' dimensions={50} fill='#088aa8' strokeColor='#053c49' strokeWidth={4} smoothness={{
+          movement: 0.2,
+          scale: 0.1,
+          opacity: 0.2,
+        }}
+        targetOpacity={0.5}
+      />
       <Simple
         Profile={() => handleScrollTo(Profileref)}
         About={() => handleScrollTo(Aboutref)}
